79e16ad4ad2466be3de9bc7d767ac08c51a1913f
Michael Malach <mmalach@ucsd.edu>
2018-05-23 17:08:49 -0700

    Got a create function working on my end along with a test file. Will add data and additional functionality but you can play with this for now

---
+++ config/connection.js
@@ -0,0 +1,29 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/flashmob_db";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("flashmob_db");
+    dbo.createCollection("USERS", function (err, res) {
+        if (err) throw err;
+        console.log("USERS Collection created!");
+    })
+    dbo.createCollection("SETS", function (err, res) {
+        if (err) throw err;
+        console.log("SETS Collection created!");
+    })
+
+    dbo.createCollection("CARDS", function (err, res) {
+        if (err) throw err;
+        console.log("CARDS Collection created!");
+    })
+    db.close();
+});
+
+module.exports = MongoClient;
---
+++ config/ormtest.js
@@ -0,0 +1,2 @@
+const orm = require('./orm')
+orm.createUser("marchosiasm", "malach", "michael", "mmalach@ucsd.edu", "randomkey2348342#*#$#$*jlsdkf")
