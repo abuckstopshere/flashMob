6f79fd3731ae3df7c0e9cbf87706bb75a1f6fd4b
Steven <steven.mezzacappa@gmail.com>
2018-05-30 13:16:52 -0700

    API Routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,130 +5,122 @@ var orm = require("../config/orm");
 module.exports = function (app) {
 
     // GET route for getting all of the todos
-    app.get("/Dashboard/User/:ID", function (req, res) {
-        orm.getUserID(req.body, function (results) {
-            res.json(results);
-        });
-    });
-    app.get("/Dashboard/User/:Sets", function (req, res) {
-        orm.getSetByAuthor(req.body, function (results) {
-            res.json(results);
-        });
-    });
-    app.get("/Set/:categories", function (req, res) {
-        orm.getSetsByCategory(req.body, function (results) {
-            res.json(results);
-        });
-    });
-    app.get(/Sets/, function (req, res) {
-        orm.getSetAll(req.body, function (results) {
-            res.json(results);
-        });
-    });
-    app.get("/Sets/:setID/Card/:cardID", function (req, res) {
-        orm.getCardsBySet(req.body, function (results) {
-            res.json(results);
-        });
+    app.get("/api/User/:ID", function (req, res) {
+        orm.getUserByID(req.params)
+            .then(user => res.json(results));
     });
 
-    // POST route for saving a new todo. We can create a todo using the data on req.body
-    app.post("/User/", function (req, res) {
-        orm.createUser(req.body, function (results) {
-            res.json(results);
-        });
+    app.get("/api/Sets/:authorID", function (req, res) {
+        orm.getSetByAuthor(req.params)
+            .then(user => res.json(results));
     });
+
+    app.get("/api/Sets/:categories", function (req, res) {
+        orm.getSetsByCategory(req.params)
+            .then(data => res.json(results));
+    });
+    app.get("/api/Sets", function (req, res) {
+        orm.getSetAll(req.params)
+            .then(data => res.json(results));
+
+    });
+    app.get("/Sets/:setID/Card/:cardID", function (req, res) {
+        orm.getCardsBySet(req.params)
+            .then(data => res.json(results));
+    });
+
+
+    // POST route for saving a user. We can create a user using the data on req.body
+    app.post("/User/", function (req, res) {
+        orm.createUser(req.params)
+            .then(user => res.json(results));
+    });
+
+    app.post("/Card/:front", function (req, res) {
+        orm.createCard(req.params)
+            .then(user => res.json(results));
+    });
+
+    app.post("/Card/:back", function (req, res) {
+        orm.createCard(req.params)
+            .then(user => res.json(results));
+    });
+
+
+    app.post("/User/:id", function (req, res) {
+        orm.createSet(req.params)
+            .then(user => res.json(results));
+    });
+
 
     // DELETE route for deleting todos. We can access the ID of the todo to delete in
     // req.params.id
     app.delete("/User/:userID", function (req, res) {
-        orm.deleteUser(req.params.userID, function (results) {
-            res.json(results);
-        });
+        orm.deleteUser(req.params)
+            .then(user => res.json(results));
     });
+
     app.delete("/Sets/:setName", function (req, res) {
-        orm.deleteSet(req.params.userID, function (results) {
-            res.json(results);
-        });
+        orm.deleteSet(req.params)
+            .then(user => res.json(results));
     });
+
     app.delete("/Sets/:setID/Card/:cardID/:front", function (req, res) {
-        orm.deleteCardByID(req.params.userID, function (results) {
-            res.json(results);
-        });
+        orm.deleteCardByID(req.params)
+            .then(user => res.json(results));
     });
+
     app.delete("/Sets/:setID/Card/:cardID/:back", function (req, res) {
-        orm.deleteCardByID(req.params.userID, function (results) {
-            res.json(results);
-        });
+        orm.deleteCardByID(req.params)
+            .then(user => res.json(results));
     });
+
 
 
     // PUT route for updating todos. We can access the updated todo in req.body
     app.put("/User/:email", function (req, res) {
-        orm.changeUserEmail(req.params, function (results) {
-            res.json(results);
-        });
+        orm.changeUserEmail(req.params)
+            .then(user => res.json(results));
     });
 
-app.put("/Set/:setID/Card/:cardID", function (req, res) {
-    orm.updateCardSet(req.params.userID, function (results) {
-        res.json(results);
-    });
-});
 
-app.put("/Card/:userID", function (req, res) {
-    orm.updateCardFields(req.params.userID, function (results) {
-        res.json(results);
-    });
-});
-
-
-app.get("/User/:ID", function (req, res) {
-        orm.getUserID(req.body, function (results) {
-            res.json(results);
-        });
+    app.put("/Set/:setID/Card/:cardID", function (req, res) {
+        orm.updateCardSet(req.params)
+            .then(user => res.json(results));
     });
 
-    // POST route for saving a new todo. We can create a todo using the data on req.body
-app.post("/User/", function (req, res) {
-        orm.createRecord(req.body, function (results) {
-            res.json(results);
-        });
+
+    app.put("/Card/:userID", function (req, res) {
+        orm.updateCardFields(req.params)
+            .then(user => res.json(results));
     });
 
-app.post("/Card/:front", function (req, res) {
-        orm.createCard(req.body, function (results) {
-            res.json(results);
-        });
-    });
-app.post("/Card/:back", function (req, res) {
-        orm.createCard(req.body, function (results) {
-            res.json(results);
-        });
+
+
+    app.get("/User/:ID", function (req, res) {
+        orm.getUserID(req.params)
+            .then(data => res.json(results));
     });
 
-app.post("/User/:id", function (req, res) {
-        orm.createSet(req.body, function (results) {
-            res.json(results);
-        });
-    }); 
 
     // DELETE route for deleting todos. We can access the ID of the todo to delete in
     // req.params.id
-app.delete("/api/:userID", function (req, res) {
-        orm.deleteUser(req.params.userID, function (results) {
-            res.json(results);
-        });
-    }); 
-app.delete("/api/:userID", function (req, res) {
-        orm.deleteUser(req.params.userID, function (results) {
-            res.json(results);
-        });
+    app.delete("/api/:userID", function (req, res) {
+        orm.deleteUser(req.params)
+            .then(data => res.json(results));
     });
 
-    // PUT route for updating todos. We can access the updated todo in req.body
-app.put("/api/:userID", function (req, res) {
-        orm.changeUserEmail(req.params.userID, function (results) {
-            res.json(results);
-        });
+    app.delete("/api/:userID", function (req, res) {
+        orm.deleteUser(req.params)
+            .then(data => res.json(results));
     });
+
+
+    // PUT route for updating todos. We can access the updated todo in req.body
+    app.put("/api/:userID", function (req, res) {
+        orm.changeUserEmail(req.params)
+            .then(data => res.json(results));
+    });
+
 };
+
--- src/components/AddSet/AddSet.jsx
+++ src/components/AddSet/AddSet.jsx
@@ -1,6 +1,10 @@
 import React , { Component } from 'react'
-import { Row , Col , Input , Button } from 'react-materialize'
+import { Row , Col , Input , Button, Icon, NavItem, Badge, Dropdown } from 'react-materialize'
 import Header from '../Header/Header'
+import Dashboard from '../Dashboard/Dashboard';
+
+
+
 // import 'AddSet.css'
 
 class AddSet extends Component {
@@ -13,6 +17,7 @@ class AddSet extends Component {
             </Row>
             <Row>
                 <Col s = {8} >
+                
                     <Input s = {6} label = "Name" />
                     <Input s = {6} label = "Categories" />
                     <Button waves='light'>Submit</Button>
