0335ef572561603b0e552b6d1f1c543054a02f19
andrew buckley <andrewbuckley@abuck-macbook.local>
2018-06-01 00:46:22 -0700

    refactored state hierarchy, less components exist now

--- src/components/Cards/AddCard/AddCard.jsx
+++ src/components/Cards/AddCard/AddCard.jsx
@@ -8,18 +8,18 @@ class AddCard extends Component {
             <div className = "col s6 offset-s3 center">
                 <h5>Add A Card</h5>
                 <div className = "row">
-                    <div class="input-field col s12">
-                        <input placeholder="Set Name" type="text" class="validate center" />
+                    <div className="input-field col s12">
+                        <input placeholder="Set Name" type="text" className="validate center" />
                     </div>
                 </div>
                 <div className = "row">
-                    <div class="input-field col s12">
-                        <input placeholder="Question" type="text" class="validate center" />
+                    <div className="input-field col s12">
+                        <input placeholder="Question" type="text" className="validate center" />
                     </div>
                 </div>
                 <div className = "row">
-                    <div class="input-field col s12">
-                        <input placeholder="Answer" type="text" class="validate center" />
+                    <div className="input-field col s12">
+                        <input placeholder="Answer" type="text" className="validate center" />
                     </div>
                 </div>
                 <button className = "btn waves-effect waves-light">Submit</button>
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React , { Component } from 'react'
+import React from 'react'
 import { Redirect } from 'react-router-dom'
 import Header from '../Header/Header'
 import CardOpts from '../Cards/CardOpts'
@@ -9,142 +9,61 @@ import AddCard from '../Cards/AddCard/AddCard'
 import ViewCard from '../Cards/ViewCard/ViewCard'
 import './Dashboard.css'
 
-class Dashboard extends Component {
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            viewSet : false ,
-            addSet : false ,
-            viewCard : false ,
-            addCard : false ,
-            dashView : true ,
-            logOut : false
-        }
-    }
-
-    handleViewCard = () => {
-        this.setState({
-            viewSet : false ,
-            addSet : false ,
-            viewCard : true ,
-            addCard : false ,
-            dashView : false ,
-            logOut : false
-        })
-    }
-
-    handleAddCard = () => {
-        this.setState({
-            viewSet : false ,
-            addSet : false ,
-            viewCard : false ,
-            addCard : true ,
-            dashView : false ,
-            logOut : false
-        })
-    }
-
-    handleViewSet = () => {
-        this.setState({
-            viewSet : true ,
-            addSet : false ,
-            viewCard : false ,
-            addCard : false ,
-            dashView : false ,
-            logOut : false
-        })
-    }
-
-    handleAddSet = () => {
-        this.setState({
-            viewSet : false ,
-            addSet : true ,
-            viewCard : false ,
-            addCard : false ,
-            dashView : false ,
-            logOut : false
-        })
-    }
-
-    handleDashView = () => {
-        this.setState({
-            viewSet : false ,
-            addSet : false ,
-            viewCard : false ,
-            addCard : false ,
-            dashView : true ,
-            logOut : false
-        })
-    }
-
-    handleLogOut = () => {
-        this.setState({
-            viewSet : false ,
-            addSet : false ,
-            viewCard : false ,
-            addCard : false ,
-            dashView : false ,
-            logOut : true
-        })
-    }
-
-    render() {
-        return (
-            <div className = "bigContainer">
-                <div className = "row">
-                    <div className = "headerNav">
-                        <Header 
-                            username = {this.props.username}
-                            welcome = {this.props.welcome}
-                            dashView = {this.state.dashView}
-                            handleDashView = {this.handleDashView}
-                            logOut = {this.state.logOut}
-                            handleLogOut =  {this.handleLogOut}
-                        />
-                    </div>
-                </div>
-                <div className = "row">
-                    {this.state.dashView ? 
-                        <div>
-                            <div className = "col s6 margerino center" >
-                                <SetOpts 
-                                    viewSet = {this.state.viewSet}
-                                    addSet = {this.state.addSet}
-                                    handleViewSet = {this.handleViewSet}
-                                    handleAddSet = {this.handleAddSet}
-                                />
-                            </div>
-                            <div className = "col s6 margerino center" >
-                                <CardOpts 
-                                    viewCard = {this.state.viewCard}
-                                    addCard = {this.state.addCard}
-                                    handleViewCard = {this.handleViewCard}
-                                    handleAddCard = {this.handleAddCard}
-                                />
-                            </div>
-                        </div>
-                    : null}
-                    {this.state.addSet ? 
-                        <AddSet />
-                    : null}
-                    {this.state.viewSet ? 
-                       <ViewSet />
-                    : null}
-                    {this.state.addCard ? 
-                        <AddCard />
-                    : null}
-                    {this.state.viewCard ? 
-                        <ViewCard />
-                    : null}
-                    {this.state.logOut ? 
-                        <Redirect to = {'/'} />
-                    : null}
+const Dashboard = (props) => {
+    return (
+        <div className = "bigContainer">
+            <div className = "row">
+                <div className = "headerNav">
+                    <Header 
+                        username = {props.username}
+                        welcome = {props.welcome}
+                        dashView = {props.dashView}
+                        handleDashView = {props.handleDashView}
+                        logOut = {props.logOut}
+                        handleLogOut =  {props.handleLogOut}
+                    />
                 </div>
             </div>
-        )
-    }
+            <div className = "row">
+                {props.dashView ? 
+                    <div>
+                        <div className = "col s6 margerino center" >
+                            <SetOpts 
+                                viewSet = {props.viewSet}
+                                addSet = {props.addSet}
+                                handleViewSet = {props.handleViewSet}
+                                handleAddSet = {props.handleAddSet}
+                            />
+                        </div>
+                        <div className = "col s6 margerino center" >
+                            <CardOpts 
+                                viewCard = {props.viewCard}
+                                addCard = {props.addCard}
+                                handleViewCard = {props.handleViewCard}
+                                handleAddCard = {props.handleAddCard}
+                            />
+                        </div>
+                    </div>
+                : null}
+                {props.addSet ? 
+                    <AddSet />
+                : null}
+                {props.viewSet ? 
+                    <ViewSet />
+                : null}
+                {props.addCard ? 
+                    <AddCard />
+                : null}
+                {props.viewCard ? 
+                    <ViewCard />
+                : null}
+                {props.logOut ? 
+                    <Redirect to = {'/'} />
+                : null}
+            </div>
+        </div>
+    )
 }
+
 
 export default Dashboard
